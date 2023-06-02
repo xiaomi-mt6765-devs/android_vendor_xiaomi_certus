1eae5afb504a04860774baf1728213b222b03d0c
Kshitij Gupta <kshitijgm@gmail.com>
2023-06-02 19:39:18 +0200

    certus: Create required vendor symlinks for gatekeeper
    Change-Id: I0d87c2b4332c0bcff0a7872fffd13e8ec1225c6e
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

--- certus-vendor.mk
+++ certus-vendor.mk
@@ -571,7 +571,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/certus/proprietary/vendor/lib/hw/audio.usb.mt6765.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/audio.usb.mt6765.so \
     vendor/xiaomi/certus/proprietary/vendor/lib/hw/audio_policy.stub.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/audio_policy.stub.so \
     vendor/xiaomi/certus/proprietary/vendor/lib/hw/flp.default.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/flp.default.so \
-    vendor/xiaomi/certus/proprietary/vendor/lib/hw/gatekeeper.default.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/gatekeeper.default.so \
     vendor/xiaomi/certus/proprietary/vendor/lib/hw/gatekeeper.mt6765.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/gatekeeper.mt6765.so \
     vendor/xiaomi/certus/proprietary/vendor/lib/hw/gps.default.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/gps.default.so \
     vendor/xiaomi/certus/proprietary/vendor/lib/hw/gralloc.default.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/gralloc.default.so \
@@ -874,7 +873,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/certus/proprietary/vendor/lib64/hw/audio_policy.stub.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/audio_policy.stub.so \
     vendor/xiaomi/certus/proprietary/vendor/lib64/hw/flp.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/flp.default.so \
     vendor/xiaomi/certus/proprietary/vendor/lib64/hw/fpc_fingerprint.mt6765.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/fpc_fingerprint.mt6765.so \
-    vendor/xiaomi/certus/proprietary/vendor/lib64/hw/gatekeeper.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/gatekeeper.default.so \
     vendor/xiaomi/certus/proprietary/vendor/lib64/hw/gatekeeper.mt6765.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/gatekeeper.mt6765.so \
     vendor/xiaomi/certus/proprietary/vendor/lib64/hw/gf_fingerprint.mt6765.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/gf_fingerprint.mt6765.so \
     vendor/xiaomi/certus/proprietary/vendor/lib64/hw/gps.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/gps.default.so \
